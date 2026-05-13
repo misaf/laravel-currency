09ce24ee6ab4e989c184e8f9deb0507a1ed0586e
Misaf <misaf.1990@gmail.com>
2026-05-13 13:06:58 +0330

    feat: add German translations

---
+++ resources/lang/de/navigation.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'currency'                     => 'Währungen',
+    'currency_category'            => 'Währungskategorien',
+    'currency_category_management' => 'Währungskategorie-Verwaltung',
+    'currency_management'          => 'Währungsverwaltung',
+];
