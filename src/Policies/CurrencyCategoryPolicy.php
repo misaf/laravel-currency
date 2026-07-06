3bcae5c513a2903d8356c31d010b44703e8b6a85
Misaf <misaf.1990@gmail.com>
2026-07-06 13:06:30 +0330

    Clean up module autoload and policies

--- src/Policies/CurrencyCategoryPolicy.php
+++ src/Policies/CurrencyCategoryPolicy.php
@@ -5,71 +5,71 @@ declare(strict_types=1);
 namespace Misaf\VendraCurrency\Policies;
 
 use Illuminate\Auth\Access\HandlesAuthorization;
+use Illuminate\Contracts\Auth\Access\Authorizable;
 use Misaf\VendraCurrency\Enums\CurrencyCategoryPolicyEnum;
 use Misaf\VendraCurrency\Models\CurrencyCategory;
-use Misaf\VendraUser\Models\User;
 
 final class CurrencyCategoryPolicy
 {
     use HandlesAuthorization;
 
-    public function create(User $user): bool
+    public function create(Authorizable $user): bool
     {
-        return $user->can(CurrencyCategoryPolicyEnum::CREATE);
+        return $user->can(CurrencyCategoryPolicyEnum::CREATE->value);
     }
 
-    public function delete(User $user, CurrencyCategory $currencyCategory): bool
+    public function delete(Authorizable $user, CurrencyCategory $currencyCategory): bool
     {
-        return $user->can(CurrencyCategoryPolicyEnum::DELETE);
+        return $user->can(CurrencyCategoryPolicyEnum::DELETE->value);
     }
 
-    public function deleteAny(User $user): bool
+    public function deleteAny(Authorizable $user): bool
     {
-        return $user->can(CurrencyCategoryPolicyEnum::DELETE_ANY);
+        return $user->can(CurrencyCategoryPolicyEnum::DELETE_ANY->value);
     }
 
-    public function forceDelete(User $user, CurrencyCategory $currencyCategory): bool
+    public function forceDelete(Authorizable $user, CurrencyCategory $currencyCategory): bool
     {
-        return $user->can(CurrencyCategoryPolicyEnum::FORCE_DELETE);
+        return $user->can(CurrencyCategoryPolicyEnum::FORCE_DELETE->value);
     }
 
-    public function forceDeleteAny(User $user): bool
+    public function forceDeleteAny(Authorizable $user): bool
     {
-        return $user->can(CurrencyCategoryPolicyEnum::FORCE_DELETE_ANY);
+        return $user->can(CurrencyCategoryPolicyEnum::FORCE_DELETE_ANY->value);
     }
 
-    public function reorder(User $user): bool
+    public function reorder(Authorizable $user): bool
     {
-        return $user->can(CurrencyCategoryPolicyEnum::REORDER);
+        return $user->can(CurrencyCategoryPolicyEnum::REORDER->value);
     }
 
-    public function replicate(User $user, CurrencyCategory $currencyCategory): bool
+    public function replicate(Authorizable $user, CurrencyCategory $currencyCategory): bool
     {
-        return $user->can(CurrencyCategoryPolicyEnum::REPLICATE);
+        return $user->can(CurrencyCategoryPolicyEnum::REPLICATE->value);
     }
 
-    public function restore(User $user, CurrencyCategory $currencyCategory): bool
+    public function restore(Authorizable $user, CurrencyCategory $currencyCategory): bool
     {
-        return $user->can(CurrencyCategoryPolicyEnum::RESTORE);
+        return $user->can(CurrencyCategoryPolicyEnum::RESTORE->value);
     }
 
-    public function restoreAny(User $user): bool
+    public function restoreAny(Authorizable $user): bool
     {
-        return $user->can(CurrencyCategoryPolicyEnum::RESTORE_ANY);
+        return $user->can(CurrencyCategoryPolicyEnum::RESTORE_ANY->value);
     }
 
-    public function update(User $user, CurrencyCategory $currencyCategory): bool
+    public function update(Authorizable $user, CurrencyCategory $currencyCategory): bool
     {
-        return $user->can(CurrencyCategoryPolicyEnum::UPDATE);
+        return $user->can(CurrencyCategoryPolicyEnum::UPDATE->value);
     }
 
-    public function view(User $user, CurrencyCategory $currencyCategory): bool
+    public function view(Authorizable $user, CurrencyCategory $currencyCategory): bool
     {
-        return $user->can(CurrencyCategoryPolicyEnum::VIEW);
+        return $user->can(CurrencyCategoryPolicyEnum::VIEW->value);
     }
 
-    public function viewAny(User $user): bool
+    public function viewAny(Authorizable $user): bool
     {
-        return $user->can(CurrencyCategoryPolicyEnum::VIEW_ANY);
+        return $user->can(CurrencyCategoryPolicyEnum::VIEW_ANY->value);
     }
 }
